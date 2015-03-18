bfbf00ed924f14bf92d62c4eafff047f68dc0f94
kumabook <kumabook@live.jp>
2015-03-18 23:51:05 +0900

    Add empty project that includes ios and mac framework target

---
+++ SoundCloudKit/SoundCloudKit/SoundCloudKit.h
@@ -0,0 +1,19 @@
+//
+//  SoundCloudKit.h
+//  SoundCloudKit
+//
+//  Created by Hiroki Kumamoto on 3/18/15.
+//  Copyright (c) 2015 Spincoaster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SoundCloudKit.
+FOUNDATION_EXPORT double SoundCloudKitVersionNumber;
+
+//! Project version string for SoundCloudKit.
+FOUNDATION_EXPORT const unsigned char SoundCloudKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SoundCloudKit/PublicHeader.h>
+
+
