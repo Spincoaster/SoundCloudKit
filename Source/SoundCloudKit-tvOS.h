512bc3497f2963e89e96e8e4f853e11da13b0dcc
Hiroki Kumamoto <kumabook@live.jp>
2016-11-03 10:14:47 +0900

    Support tvOS and watchOS

---
+++ Source/SoundCloudKit-tvOS.h
@@ -0,0 +1,19 @@
+//
+//  SoundCloudKit-tvOS.h
+//  SoundCloudKit-tvOS
+//
+//  Created by Hiroki Kumamoto on 11/3/16.
+//  Copyright © 2016 Spincoaster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SoundCloudKit-tvOS.
+FOUNDATION_EXPORT double SoundCloudKitVersionNumber;
+
+//! Project version string for SoundCloudKit-tvOS.
+FOUNDATION_EXPORT const unsigned char SoundCloudKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SoundCloudKit/PublicHeader.h>
+
+
